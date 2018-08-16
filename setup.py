026716f57b2fc79aedab8ab4c4a4a209834c282e
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2018-08-16 14:38:29 -0500

    added cer3 and cer6

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 setup(
     name='lipids',
     version='0.0.0',
-    author='Timothy C. Moore',
-    author_email='tcmoore3@gmail.com',
+    author='Timothy C. Moore and Parashara Shamaprasad',
+    author_email='p.shama@vanderbilt.edu',
     packages=find_packages(),
-    url='http://github.com/tcmoore3/lipids',
+    url='http://github.com/uppittu11/lipids',
     description='My lipid molecules for building lipid systems.',
     long_description=open('README.md').read(),
     install_requires=[
